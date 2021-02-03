e1550cd284c71d9740bb8eba809a86c375d00aa2
Berkan Senger <sengerberkann@gmail.com>
2021-02-03 23:50:23 +0300

    Add project files.

---
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+
+
+    public interface ICategoryDal:IEntityRepository<Category> //Public önemlidir.Diğer katmanlardan erişimi sağlar (referans verdiğimiz zaman)
+    {
+        //Generic altyapı
+        //List<Category> GetAll();
+        //void Add(Category category);
+        //void Update(Category category);
+        //void Delete(Category category);
+
+        //List<Category> GetAllByCategory(int categoryId);
+    }
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint 
+    //class : Referans tip 
+    //classların hepsinin ortak özelliği IEntity olması
+    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
+    //new() : new'lenebilir olmalı
+    //IEntity new'lenemez. (Soyut bir nesne)
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        //List<T> GetAllByCategory(int categoryId); 10.satırdakini yazdıktan sonra ihtiyacımız kalmaz
+    }
+}
